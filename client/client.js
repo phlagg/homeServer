bb5da79ba973c26186d645751043ceebce2d9b51
Michael <michaelthomaswoods@gmail.com>
2018-11-30 19:12:12 -0800

    added buttons

---
+++ client/client.js
@@ -0,0 +1,11 @@
+console.log("Hello");
+const form = document.querySelector('form');
+   
+form.addEventListener('click', (event) => {
+    console.log("BUZZZZZ");
+});
+
+// var b = document.getElementById('buzz');
+// b.onclick = function() {
+//     console.log("BUZZZZZ");
+// };
