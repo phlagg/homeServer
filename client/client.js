3b45cc6af446c15f5e3705290f46f0a8b7292fb2
Michael <michaelthomaswoods@gmail.com>
2018-12-01 15:19:33 -0800

    Added buttons

--- client/client.js
+++ client/client.js
@@ -1,7 +1,32 @@
 console.log("Hello");
-const form = document.querySelector('form');
+const form = document.querySelector('buzz_off');
+
+var mcuSocket = new WebSocket("ws:192.168.0.10:81");
    
 form.addEventListener('click', (event) => {
-    console.log("BUZZZZZ");
+    
+    buzzNow();
+    
 });
 
+
+
+// exampleSocket.close();
+
+function buzzNow() {
+    console.log("BUZZZZZ");
+    mcuSocket.send("#0");
+    console.log("UNBUZZZZZ");
+    mcuSocket.send("#3ff");
+}
+
+function buzzOn() {
+    console.log("BUZZZZZ");
+    mcuSocket.send("#0");
+   
+}
+function buzzOff() {
+   
+    console.log("UNBUZZZZZ");
+    mcuSocket.send("#3ff");
+}
